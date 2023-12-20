43fc698b2f66b5ba1b1311a77674ad5444238abe
abdellah <Abdellah.chehri14@gmail.com>
2023-12-20 05:54:06 +0100

    push to main

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="max-container">
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
-          <Image src="/assets/brand.png" alt="logo" width={150} height={80} />
+          <Image src="/brand.png" alt="logo" width={150} height={80} />
 
           <p className="mt-6 text-base leading-7 text-white font-montserrat text-white-400 sm:max-w-sm">
             Get shoes ready for the new term at your nearest Nike store. Find
